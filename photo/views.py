95353f3a0b90e8f93ba0f9fb4d8b2f259b48831e
helga-helga <olyalavyagina@gmail.com>
2015-12-13 19:53:48 +0200

    Added big and small images in album view

--- photo/views.py
+++ photo/views.py
@@ -34,14 +34,17 @@ def main(request):
     return render_to_response("photo/list.html", dict(albums=albums, user=request.user))
 
 
-def album(request, pk):
+def album(request, pk, view="thumbnail"):
     """Album listing."""
+    num_images = 30
+    if view == "full":
+        num_images = 10
 
     album = Album.objects.get(pk=pk)
     if not album.public and not request.user.is_authenticated():
         return HttpResponse("Error: you need to be logged in to view this album.")
     images = album.image_set.all()
-    paginator = Paginator(images, 30)
+    paginator = Paginator(images, num_images)
     try:
         page = int(request.GET.get("page", '1'))
     except ValueError:
@@ -52,7 +55,7 @@ def album(request, pk):
     except (InvalidPage, EmptyPage):
         images = paginator.page(paginator.num_pages)
 
-    return render_to_response("photo/album.html", dict(album=album, images=images, user=request.user,
+    return render_to_response("photo/album.html", dict(album=album, images=images, user=request.user, view=view,
                                                        media_url=MEDIA_URL))
 
 
